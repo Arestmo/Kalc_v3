078828ac9f0e2b2dc17d0334f59535f0d0e92b4f
MateuszT <mateusz@clearmedia.pl>
2020-01-30 23:52:24 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Kalc_v3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button_calc = new System.Windows.Forms.Button();
+            this.Check_add = new System.Windows.Forms.CheckBox();
+            this.Check_substract = new System.Windows.Forms.CheckBox();
+            this.Check_multiply = new System.Windows.Forms.CheckBox();
+            this.Check_divide = new System.Windows.Forms.CheckBox();
+            this.Result_text = new System.Windows.Forms.TextBox();
+            this.Text_val1 = new System.Windows.Forms.TextBox();
+            this.Text_val2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button_calc
+            // 
+            this.Button_calc.Location = new System.Drawing.Point(225, 34);
+            this.Button_calc.Name = "Button_calc";
+            this.Button_calc.Size = new System.Drawing.Size(75, 69);
+            this.Button_calc.TabIndex = 0;
+            this.Button_calc.Text = "Calc";
+            this.Button_calc.UseVisualStyleBackColor = true;
+            this.Button_calc.Click += new System.EventHandler(this.Button_calc_Click);
+            // 
+            // Check_add
+            // 
+            this.Check_add.AutoSize = true;
+            this.Check_add.Location = new System.Drawing.Point(12, 63);
+            this.Check_add.Name = "Check_add";
+            this.Check_add.Size = new System.Drawing.Size(45, 17);
+            this.Check_add.TabIndex = 1;
+            this.Check_add.Text = "Add";
+            this.Check_add.UseVisualStyleBackColor = true;
+            this.Check_add.CheckedChanged += new System.EventHandler(this.Check_add_CheckedChanged);
+            // 
+            // Check_substract
+            // 
+            this.Check_substract.AutoSize = true;
+            this.Check_substract.Location = new System.Drawing.Point(12, 86);
+            this.Check_substract.Name = "Check_substract";
+            this.Check_substract.Size = new System.Drawing.Size(71, 17);
+            this.Check_substract.TabIndex = 2;
+            this.Check_substract.Text = "Substract";
+            this.Check_substract.UseVisualStyleBackColor = true;
+            this.Check_substract.CheckedChanged += new System.EventHandler(this.Check_substract_CheckedChanged);
+            // 
+            // Check_multiply
+            // 
+            this.Check_multiply.AutoSize = true;
+            this.Check_multiply.Location = new System.Drawing.Point(119, 63);
+            this.Check_multiply.Name = "Check_multiply";
+            this.Check_multiply.Size = new System.Drawing.Size(61, 17);
+            this.Check_multiply.TabIndex = 3;
+            this.Check_multiply.Text = "Multiply";
+            this.Check_multiply.UseVisualStyleBackColor = true;
+            this.Check_multiply.CheckedChanged += new System.EventHandler(this.Check_multiply_CheckedChanged);
+            // 
+            // Check_divide
+            // 
+            this.Check_divide.AutoSize = true;
+            this.Check_divide.Location = new System.Drawing.Point(119, 86);
+            this.Check_divide.Name = "Check_divide";
+            this.Check_divide.Size = new System.Drawing.Size(56, 17);
+            this.Check_divide.TabIndex = 4;
+            this.Check_divide.Text = "Divide";
+            this.Check_divide.UseVisualStyleBackColor = true;
+            this.Check_divide.CheckedChanged += new System.EventHandler(this.Check_divide_CheckedChanged);
+            // 
+            // Result_text
+            // 
+            this.Result_text.Location = new System.Drawing.Point(12, 109);
+            this.Result_text.Multiline = true;
+            this.Result_text.Name = "Result_text";
+            this.Result_text.Size = new System.Drawing.Size(288, 75);
+            this.Result_text.TabIndex = 5;
+            // 
+            // Text_val1
+            // 
+            this.Text_val1.Location = new System.Drawing.Point(13, 37);
+            this.Text_val1.Name = "Text_val1";
+            this.Text_val1.Size = new System.Drawing.Size(100, 20);
+            this.Text_val1.TabIndex = 6;
+            // 
+            // Text_val2
+            // 
+            this.Text_val2.Location = new System.Drawing.Point(119, 37);
+            this.Text_val2.Name = "Text_val2";
+            this.Text_val2.Size = new System.Drawing.Size(100, 20);
+            this.Text_val2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Value 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Value 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 200);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Text_val2);
+            this.Controls.Add(this.Text_val1);
+            this.Controls.Add(this.Result_text);
+            this.Controls.Add(this.Check_divide);
+            this.Controls.Add(this.Check_multiply);
+            this.Controls.Add(this.Check_substract);
+            this.Controls.Add(this.Check_add);
+            this.Controls.Add(this.Button_calc);
+            this.Name = "Form1";
+            this.Text = "Calc v3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_calc;
+        private System.Windows.Forms.CheckBox Check_add;
+        private System.Windows.Forms.CheckBox Check_substract;
+        private System.Windows.Forms.CheckBox Check_multiply;
+        private System.Windows.Forms.CheckBox Check_divide;
+        private System.Windows.Forms.TextBox Result_text;
+        private System.Windows.Forms.TextBox Text_val1;
+        private System.Windows.Forms.TextBox Text_val2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalc_v3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String result = "";
+
+        private void Check_add_CheckedChanged(object sender, EventArgs e)
+        {
+            if(Check_add.Checked)
+            {
+                int val1 = Get_value(Text_val1);
+                int val2 = Get_value(Text_val2);
+
+                //Place your code here
+            }
+        }
+
+        private void Check_substract_CheckedChanged(object sender, EventArgs e)
+        {
+            if(Check_substract.Checked)
+            {
+                int val1 = Get_value(Text_val1);
+                int val2 = Get_value(Text_val2);
+
+                //Place your code here
+            }
+        }
+
+        private void Check_multiply_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Check_multiply.Checked)
+            {
+                int val1 = Get_value(Text_val1);
+                int val2 = Get_value(Text_val2);
+
+                //Place your code here
+            }
+        }
+
+        private void Check_divide_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Check_divide.Checked)
+            {
+                int val1 = Get_value(Text_val1);
+                int val2 = Get_value(Text_val2);
+
+                //Place your code here
+            }
+        }
+
+        private void Button_calc_Click(object sender, EventArgs e)
+        {
+            Result_text.Text = result;
+        }
+
+        private int Get_value(TextBox text)
+        {
+            int value = int.Parse(text.Text);
+            return value;
+        }
+    }
+}
